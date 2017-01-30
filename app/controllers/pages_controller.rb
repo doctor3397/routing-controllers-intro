6b99b4c2636e37a2d06639b477a72fb3f5829f30
doctor3397 <hs2546@columbia.edu>
2017-01-30 13:24:37 -0500

    Define @header in welcome, about and contest methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def welcome
+     @header = "This is the header defiend in the welcome method rendered by the application.html.erb"
   end
 
   def about
+    @header = "This is the header defiend in the about method rendered by the application.html.erb"
   end
 
   def contest
+    @header = "This is the header defiend in the contest method rendered by the application.html.erb"
 
   end
 end
