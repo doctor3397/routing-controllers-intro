b76858ad5af44e49059f53524d07eea12da356b2
doctor3397 <hs2546@columbia.edu>
2017-01-30 14:15:39 -0500

    Add a '/secrets/:magic' route and a secrets method in pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   # Allow the user to specify what size of image they would like by defining a dynamic route.
   get '/kitten/:size' => 'pages#kitten'
   get '/kittens/:size' => 'pages#kittens'
+
+  # Conditional Redirects, redirect our user away from a page only if they aren't authorized to see it
+  get '/secrets/:magic_word' => 'pages#secrets'
 end
