c95b271bfbf18f9c410f1167c8d40172b86f17eb
doctor3397 <hs2546@columbia.edu>
2017-01-30 13:33:32 -0500

    Add the kitten/:size route and kitten action in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,11 @@ class PagesController < ApplicationController
 
   def contest
     @header = "This is the header defiend in the contest method rendered by the application.html.erb"
+  end
 
+  def kitten
+    @header = "This is the header defiend in the kitten method rendered by the application.html.erb"
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/contest' => 'pages#contest'
+
+  # Allow the user to specify what size of image they would like by defining a dynamic route.
+  get '/kitten/:size' => 'pages#kitten'
 end
